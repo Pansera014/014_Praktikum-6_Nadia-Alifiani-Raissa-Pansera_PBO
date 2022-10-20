7bfdd0f280a55ee691cea9cab192303133b5f3d1
Nadia Alifiani Raissa Pansera <99786410+Pansera014@users.noreply.github.com>
2022-10-20 22:15:25 +0700

    Add files via upload

---
+++ Praktikum 6/No 1/No1.php
@@ -0,0 +1,39 @@
+<--- Nadia Alifiani Raissa Pansera ; 21091397014 ; 2021B --->
+
+<?php 
+
+require_once 'abstract.php';
+
+class Truck extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+    
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+        
+        return ceil($fuel /= $trip);
+    }
+}
+
+class RiverBarge extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+
+        return ceil($fuel /= $trip);
+    }
+}
+$truck = new Truck(120000, 'Truk');
+$riverBarge = new RiverBarge(18000, 'Perahu');
---
+++ Praktikum 6/No 3/interface.php
@@ -0,0 +1,14 @@
+<--- Nadia Alifiani Raissa Pansera ; 21091397014 ; 2021B --->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
